165cd4d761c1168d2d026f80c69121f215be97f1
Shaun Luttin <admin@shaunluttin.com>
2016-03-20 18:20:14 -0700

    Add runtime info project.

---
+++ RuntimeInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync(
+                    System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription);
+            });
+        }
+        
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseServer("Microsoft.AspNetCore.Server.Kestrel")
+                        .UseIISPlatformHandlerUrl()
+                        .UseStartup<Program>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
