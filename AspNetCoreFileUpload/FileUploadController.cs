720d081e044fb68e0712d40a0ce2683a6857c1b9
Shaun Luttin <admin@shaunluttin.com>
2016-04-05 20:39:35 -0700

    Attempt stream upload large file.

---
+++ AspNetCoreFileUpload/FileUploadController.cs
@@ -0,0 +1,44 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AspNetCoreFileUpload.Controllers
+{
+    public class FileUploadController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Index(IFormFile myFile)
+        {
+            var fileName = GetFileName(myFile);
+            // await SaveAsync(myFile, fileName);
+
+            var result = new { files = fileName };
+            return new OkObjectResult(result);
+        }
+
+        private string GetFileName(IFormFile file)
+        {
+            return file.ContentDisposition
+                            .Split(';')
+                            .SingleOrDefault(part => part.Contains("filename"))
+                            .Split('=')
+                            .Last()
+                            .Trim('"');
+        }
+
+        private async Task SaveAsync(IFormFile file, string fileName)
+        {
+            var bufferSize = 1024;
+            using (var fileStream = System.IO.File.Create(fileName, bufferSize, FileOptions.Asynchronous))
+            {
+                var inputStream = file.OpenReadStream();
+                await inputStream.CopyToAsync(fileStream, bufferSize);
+            }
+        }
+    }
+}
---
+++ AspNetCoreFileUpload/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.ToString().Contains("FileUpload"))
+                {
+                    await next.Invoke();
+                }
+                else
+                {
+                    await context.Response.WriteAsync(@"
+<form action = ""/FileUpload"" method=""post"" enctype=""multipart/form-data"">
+<label for=""myfile1"">File</label>
+<input type=""file"" name=""myFile"" />
+<input type=""submit"" value=""Send"" />
+</form>");
+                }
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=FileUpload}/{action=index}/{id?}"
+                );
+
+            });
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseServer("Microsoft.AspNetCore.Server.Kestrel")
+                        .UseStartup<Program>()
+                        .Build();
+
+            host.Run();
+        }
+    }
+}
