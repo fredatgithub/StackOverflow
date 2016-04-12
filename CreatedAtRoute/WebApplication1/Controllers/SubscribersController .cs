6a40104059b193a1aad44bf44e60c3da877dbd10
Shaun Luttin <admin@shaunluttin.com>
2016-04-11 17:38:49 -0700

    Add a created at route demo.

---
+++ CreatedAtRoute/WebApplication1/Controllers/SubscribersController .cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.AspNet.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    public class SubscribersController : Controller
+    {
+        public IActionResult Index()
+        {
+            var subscriber = new
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Shaun",
+                LastName = "Luttin"
+            };
+
+            return CreatedAtRoute(
+                routeName: "SubscriberLink",
+                routeValues: new { id = subscriber.Id },
+                value: subscriber);
+        }
+
+        [HttpGet("{id}", Name = "SubscriberLink")]
+        public IActionResult GetSubscriber(Guid id)
+        {
+            var subscriber = new
+            {
+                Id = id,
+                FirstName = "Shaun",
+                LastName = "Luttin"
+            };
+
+            return new JsonResult(subscriber);
+        }
+    }
+}
---
+++ CreatedAtRoute/WebApplication1/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
