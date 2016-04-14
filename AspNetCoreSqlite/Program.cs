6ebdb567d323bd7aa1087d1766e20ee7a76b06b4
Shaun Luttin <admin@shaunluttin.com>
2016-04-14 14:54:19 -0700

    Ingore approot.

--- AspNetCoreSqlite/Program.cs
+++ AspNetCoreSqlite/Program.cs
@@ -1,21 +1,50 @@
 ﻿using System;
 using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
-    public class Program
+    public class Startup
     {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context =>
+            {
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+
         public static void Main(string[] args)
         {
             var contentRoot = Directory.GetCurrentDirectory();
             var webRoot = Path.Combine(contentRoot, "wwwroot");
             var dataRoot = Path.Combine(contentRoot, "appdata");
-            
+
             Console.WriteLine("---");
             Console.WriteLine($"contentRoot:{contentRoot}");
             Console.WriteLine($"webRoot:{webRoot}");
             Console.WriteLine($"dataRoot:{dataRoot}");
             Console.WriteLine("---");
+
+            // var host = new WebHostBuilder()
+            //     .UseKestrel()
+            //     .UseContentRoot(Directory.GetCurrentDirectory())
+            //     .UseDefaultHostingConfiguration(args)
+            //     .UseIISIntegration()
+            //     .UseStartup<Startup>()
+            //     .Build();
+
+            // host.Run();
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseIISPlatformHandlerUrl()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
         }
     }
 }
