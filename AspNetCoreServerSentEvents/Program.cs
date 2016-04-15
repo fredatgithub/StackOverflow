09175f9d310421e010ca34a52872efa30bee5d9e
Shaun Luttin <admin@shaunluttin.com>
2016-04-15 11:22:53 -0700

    Add kestrel.

--- AspNetCoreServerSentEvents/Program.cs
+++ AspNetCoreServerSentEvents/Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ConsoleApplication
 {
     public class Program
     {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(context => {
+               return context.Response.WriteAsync($"Hello world!"); 
+            }); 
+        }
+        
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+
+            host.Run();
         }
     }
 }
