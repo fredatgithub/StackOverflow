ff86df2933837d3f8380009ba904bfe3431016aa
Shaun Luttin <admin@shaunluttin.com>
2016-04-15 13:22:43 -0700

    Send three messages.

--- AspNetCoreServerSentEvents/Program.cs
+++ AspNetCoreServerSentEvents/Program.cs
@@ -4,8 +4,9 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
-namespace ConsoleApplication
+namespace ServerSentEventSample 
 {
     public class Program
     {
@@ -13,10 +14,9 @@ namespace ConsoleApplication
         {
             services.AddDirectoryBrowser();
         }
+        
         public void Configure(IApplicationBuilder app, IHostingEnvironment host)
         {
-            Console.WriteLine(host.WebRootPath);
-
             app.UseFileServer(new FileServerOptions
             {
                 EnableDirectoryBrowsing = true
@@ -28,6 +28,9 @@ namespace ConsoleApplication
                 if (path.Contains("sse"))
                 {
                     context.Response.Headers.Add("Content-Type", "text/event-stream");
+                    await context.Response.WriteAsync("data: First message.\r\n");
+                    await context.Response.WriteAsync("data: Second message.\r\n");
+                    await context.Response.WriteAsync("data: Third message.\r\n\r\n");
                 }
 
                 await Task.Delay(1);
