c772fba11024c2da9aca100531cfa11f52fee53c
Shaun Luttin <admin@shaunluttin.com>
2016-04-15 15:59:00 -0700

    Remove dependency on threading.

--- AspNetCoreServerSentEvents/Program.cs
+++ AspNetCoreServerSentEvents/Program.cs
@@ -3,7 +3,6 @@ using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
-using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
