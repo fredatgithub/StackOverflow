937b13d1a720b9bc4d1dd3662d09cde8ee8b3c89
Shaun Luttin <admin@shaunluttin.com>
2016-04-17 20:29:38 -0700

    Cleanup names.

--- AspNetCoreCustomControllerNames/MyControllerFeatureProvider.cs
+++ AspNetCoreCustomControllerNames/MyControllerFeatureProvider.cs
@@ -8,15 +8,15 @@ namespace CustomControllerNames
     {
         protected override bool IsController(TypeInfo typeInfo)
         {
-            Console.WriteLine("IsController: " + typeInfo.Name);
-            
             var isController = base.IsController(typeInfo);
             if (!isController)
             {
-                isController = typeInfo.Name.EndsWith(
-                    "Controller`1", 
-                    StringComparison.OrdinalIgnoreCase);
+                isController = 
+                    typeInfo.Name.EndsWith("Controller`1", StringComparison.OrdinalIgnoreCase) ||
+                    typeInfo.Name.EndsWith("Foobar", StringComparison.OrdinalIgnoreCase);
             }
+            
+            Console.WriteLine($"{typeInfo.Name} IsController: {isController}.");
             
             return isController;
         }
