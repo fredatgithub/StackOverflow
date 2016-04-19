490013c46526ed49d6e209ba31d4273530002f89
Shaun Luttin <admin@shaunluttin.com>
2016-04-19 08:18:10 -0700

    Add middleware redirect.

---
+++ AspNetCoreMiddlewareRedirect/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MyXmlSample
+{
+    public class Program
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore();
+        }
+        
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                // if specific condition does not meet
+                if (context.Request.Path.ToString().Equals("/foo"))
+                {
+                    context.Response.Redirect("http://www.google.com");
+                }
+            });
+            
+            app.UseMvc();
+        }
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Program>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
