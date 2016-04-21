f4c5f27f61899f0c8bf87ddea3680c420f924e50
Shaun Luttin <admin@shaunluttin.com>
2016-04-21 10:52:50 -0700

    Add middleware class.

--- AspNetCoreMiddlewareRedirect/Program.cs
+++ AspNetCoreMiddlewareRedirect/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,9 +12,10 @@ namespace MiddlewareRedirectSample
         {
             services.AddMvcCore();
         }
-        
+
         public void Configure(IApplicationBuilder app)
         {
+            // inline middleware
             app.Use(async (context, next) =>
             {
                 // if specific condition does not meet
@@ -21,8 +23,15 @@ namespace MiddlewareRedirectSample
                 {
                     context.Response.Redirect("http://www.google.com");
                 }
+                else
+                {
+                    await next.Invoke();
+                }
             });
-            
+
+            // middleware class
+            app.UseMiddleware<RedirectMiddleware>();
+
             app.UseMvc();
         }
 
@@ -34,6 +43,29 @@ namespace MiddlewareRedirectSample
                 .Build();
 
             host.Run();
+        }
+    }
+
+    public class RedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            // if specific condition does not meet
+            if (context.Request.Path.ToString().Equals("/bar"))
+            {
+                context.Response.Redirect("http://www.google.com");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
         }
     }
 }
