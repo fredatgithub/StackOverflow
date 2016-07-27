44959f27cafbeb7d3caf9fc4c33398e3aa6f14ac
Shaun Luttin <admin@shaunluttin.com>
2016-07-26 20:44:48 -0700

    Serve mutlipart/byteranges to Firefox.

--- AspNetCoreMultipartByterRanges/Startup.cs
+++ AspNetCoreMultipartByterRanges/Startup.cs
@@ -1,36 +1,55 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System.IO;
 using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 
 public class Startup
 {
     private const string CrLf = "\r\n";
+    private const string Boundary = "--THIS_STRING_SEPARATES";
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+    }
 
     public void Configure(IApplicationBuilder app)
     {
         app.Run(async context =>
         {
             var response = context.Response;
-            response.Headers.Add("Content-Type", "multipart/byteranges; boundary=THIS_STRING_SEPARATES");
+            response.ContentType = $"multipart/byteranges; boundary={Boundary}";
 
-            if(!File.Exists("./goats01.jpg"))
-            {
-                throw new Exception();
-            }
-
-            var bytes = File.ReadAllBytes("./goats01.jpg");
-            var file = new FileContentResult(bytes, "image/jpg");
-
-            context.Response.ContentType = file.ContentType.ToString();
-
-            await context.Response.Body.WriteAsync(
-                file.FileContents, 
-                offset:0, 
-                count: file.FileContents.Length);
-
+            await response.WriteAsync(Boundary + CrLf);
+            await AddImage(response, "./blue.jpg");
+            await AddImage(response, "./red.jpg");
+            await AddImage(response, "./green.jpg");
             response.Body.Flush();
         });
+    }
+
+    private async Task AddImage(HttpResponse response, string path)
+    {
+        if (!File.Exists(path))
+        {
+            throw new Exception();
+        }
+
+        var bytes = File.ReadAllBytes(path);
+        var file = new FileContentResult(bytes, "image/jpg");
+
+        await response.WriteAsync("Content-type: " + file.ContentType.ToString() + CrLf);
+
+        // TODO Add Content-range header. This will take some thought. :)
+
+        await response.WriteAsync(CrLf);
+        await response.Body.WriteAsync(
+            file.FileContents, 
+            offset: 0, 
+            count: file.FileContents.Length);
+        await response.WriteAsync(CrLf);
+        await response.WriteAsync(Boundary + CrLf);
     }
 }
