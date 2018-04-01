8fa169e887c73252967b8c1ab1eb50bf1cb45977
Shaun Luttin <shaunluttin@gmail.com>
2018-03-31 23:07:31 -0700

    Add ConfigureAwait demo

---
+++ DotNetAsyncConfigureAwait/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConfigureAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Run().GetAwaiter().GetResult();
+        }
+
+        static async Task Run()
+        {
+            var syncContext = new MySyncContext { State = "The Original Context" };
+            SynchronizationContext.SetSynchronizationContext(syncContext);
+
+            Console.WriteLine("Original:" + SynchronizationContext.Current);
+
+            await Task.Delay(1000).ConfigureAwait(true);
+            Console.WriteLine("After Result1:" + SynchronizationContext.Current);
+
+            await Task.Delay(1000);
+            Console.WriteLine("After Result2:" + SynchronizationContext.Current);
+        }
+    }
+
+    public class MySyncContext : SynchronizationContext
+    {
+        public string State { get; set; }
+
+        public override void Post(SendOrPostCallback callback, object state)
+        {
+            base.Post(s => { 
+                SynchronizationContext.SetSynchronizationContext(this);
+                callback(s);
+            }, state);
+        }
+
+        public override string ToString() => State;
+    }
+}
