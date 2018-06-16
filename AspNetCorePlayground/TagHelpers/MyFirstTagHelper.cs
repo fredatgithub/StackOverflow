afa5d13c34d848e22bbda72538aa44f778a160b1
Shaun Luttin <shaunluttin@gmail.com>
2018-06-15 21:09:31 -0700

    Add TagHelper properties

--- AspNetCorePlayground/TagHelpers/MyFirstTagHelper.cs
+++ AspNetCorePlayground/TagHelpers/MyFirstTagHelper.cs
@@ -1,4 +1,4 @@
-
+using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
@@ -6,31 +6,50 @@ namespace AspNetCorePlayground.TagHelpers
 {
     public class MyFirst : TagHelper
     {
+        public string MyStringProperty { get; set; }
+
+        public DateTime MyDateTimeProperty { get; set; }
+
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
-            // get the existing content
+            ModifyTag(output);
+            ModifyAttributes(output);
+            ModifyContents(output);
+
+            // get the markup content from the .cshtml file
             var childContent = await output.GetChildContentAsync();
             var innerHtml = childContent.GetContent();
 
-            // wrap all the PreContent, Content, and PostContent in a paragraph tag.
-            output.TagName = "p";
-            output.TagMode = TagMode.StartTagAndEndTag; // optional
-
-            ModifyTagHelperContent(output);
+            output.Content.AppendHtml($"<li>{innerHtml}");
+            output.Content.AppendHtml($"<li><mark>{MyStringProperty}</mark>");
+            output.Content.AppendHtml($"<li><mark>{MyDateTimeProperty.ToString("D")}</mark>");
         }
 
-        public void ModifyTagHelperContent(TagHelperOutput output)
+        public void ModifyTag(TagHelperOutput output)
+        {
+            // wrap all the PreContent, Content, and PostContent in a paragraph tag.
+            output.TagName = "ul";
+            output.TagMode = TagMode.StartTagAndEndTag; // optional
+        }
+
+        public void ModifyAttributes(TagHelperOutput output)
+        {
+            output.Attributes.Add("class", "small text-primary");
+        }
+
+        public void ModifyContents(TagHelperOutput output)
         {
             // outside of the TagName tag
-            output.PreElement.SetHtmlContent("<p>PreElement.SetHtmlContent</p>");
-            output.PostElement.SetHtmlContent("<p>PostElement.SetHtmlContent</p>");
+            output.PreElement.SetHtmlContent("<p>PreElement.SetHtmlContent");
+            output.PostElement.SetHtmlContent("<p>PostElement.SetHtmlContent");
 
             // inside the TagName tag
-            output.PreContent.SetHtmlContent("<i>PreContent.SetHtmlContent</i>, ");
-            output.PostContent.SetHtmlContent("<i>PostContent.SetHtmlContent</i>.");
-            output.Content.SetContent("Content.SetContent, "); // replaces existing content
-            output.Content.Append("Content.Append, "); 
-            output.Content.AppendHtml("<i>Content.AppendHtml</i>, ");
+            output.PreContent.SetHtmlContent("<li>PreContent.SetHtmlContent");
+            output.PostContent.SetHtmlContent("<li>PostContent.SetHtmlContent");
+
+            output.Content
+                .SetHtmlContent("<li>Content.SetHtmlContent") // replaces existing content
+                .AppendHtml("<li>Content.AppendHtml");
         }
     }
 }
