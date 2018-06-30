fa6f61bd326bc71e39a67177233057f4ea708a58
Shaun Luttin <shaunluttin@gmail.com>
2018-06-29 19:56:21 -0700

    Delete comment

--- AspNetCoreUriToAssoc/Startup.cs
+++ AspNetCoreUriToAssoc/Startup.cs
@@ -13,7 +13,6 @@ namespace AspNetCoreUriToAssoc
                     options.ValueProviderFactories.Add(new PathValueProviderFactory()));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseMvc(routes => {
